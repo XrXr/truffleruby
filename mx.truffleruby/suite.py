1dbe61ff8a3fb870c714b5a70a332b83fcb33870
Chris Seaton <chris.seaton@shopify.com>
2019-11-07 15:43:04 -0500

    Use the thermometer

--- mx.truffleruby/suite.py
+++ mx.truffleruby/suite.py
@@ -9,7 +9,7 @@ suite = {
                 "name": "tools",
                 "subdir": True,
                 # version must always be equal to the version of the "sulong" import below
-                "version": "0c01efd4c243edc78f586fb61178436ed5f3f0fe",
+                "version": "d534d2d23b6df19418587003db9ef0b6e21fd075",
                 "urls": [
                     {"url": "git@github.com:Shopify/graal-shopify.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
@@ -19,7 +19,7 @@ suite = {
                 "name": "sulong",
                 "subdir": True,
                 # version must always be equal to the version of the "tools" import above
-                "version": "0c01efd4c243edc78f586fb61178436ed5f3f0fe",
+                "version": "d534d2d23b6df19418587003db9ef0b6e21fd075",
                 "urls": [
                     {"url": "git@github.com:Shopify/graal-shopify.git", "kind": "git"},
                     {"url": "https://curio.ssw.jku.at/nexus/content/repositories/snapshots", "kind": "binary"},
